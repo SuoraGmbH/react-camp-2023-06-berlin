0bc414b6c3af28bbd83a5917bab8cac06ae85dae
Hans-Christian Otto <c.otto@suora.com>
2023-06-26 16:16:13 +0200

    introduce prop for timeentry

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 import TimeEntryView from "./components/TimeEntryView";
 import React from "react";
+import TimeEntry from "./domain/TimeEntry";
 
 export default function App() {
-  const timeEntryViewProps: React.ComponentProps<typeof TimeEntryView> = {
-    comment: "React ist knorke!",
+  const timeEntry: TimeEntry = {
+    start: new Date(),
+    end: new Date(),
+    comment: "Hallo Welt",
+    id: "sdhiaj9das9udjsaj9ußdsa",
   };
 
   return (
     <>
       <h1>Hallo react-camp!</h1>
-      <TimeEntryView {...timeEntryViewProps} />
+      <TimeEntryView timeEntry={timeEntry} />
     </>
   );
 }
