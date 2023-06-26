fb6ecf2f241dc7c9d69d2fa79d3d07d4d94ff69e
Hans-Christian Otto <c.otto@suora.com>
2023-06-26 11:20:08 +0200

    switch to react 18 syntax

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,13 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 
-const element = <h1>Hallo Hyperion!</h1>
+const element = <h1>Hallo Hyperion!!!</h1>
+const container = document.getElementById('root');
 
-ReactDOM.render(element, document.getElementById('root'))
+if(container === null) {
+    alert("Hey, mir fehlt mein Container, wohin soll ich rendern?!")
+} else {
+    ReactDOM
+        .createRoot(container)
+        .render(element)
+}
