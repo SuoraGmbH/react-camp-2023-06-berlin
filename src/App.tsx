f6ba49c43793b8d83dba5cecd15c0a834755282d
Hans-Christian Otto <c.otto@suora.com>
2023-06-27 14:01:51 +0200

    cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -6,15 +6,6 @@ import Counter from "./components/Counter";
 import TimeEntryList from "./components/TimeEntryList";
 
 export default function App() {
-  // So können wir den typen *einer* bestimmten Prop inferieren
-  // const timeEntry: React.ComponentProps<typeof TimeEntryView>["timeEntry"] = {
-  const timeEntry: TimeEntry = {
-    start: new Date(),
-    end: new Date(),
-    comment: "Hallo Welt",
-    id: "sdhiaj9das9udjsaj9ußdsa",
-  };
-
   const [timeEntries, setTimeEntries] = useState<TimeEntry[]>([]);
 
   const handleAddTimeEntry = (timeEntry: TimeEntry) => {
@@ -23,7 +14,6 @@ export default function App() {
 
   return (
     <>
-      <TimeEntryView timeEntry={timeEntry} />
       <TimeEntryForm onAddTimeEntry={handleAddTimeEntry} />
       <TimeEntryList timeEntries={timeEntries} />
     </>
