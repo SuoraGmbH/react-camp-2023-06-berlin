f7c0c9dd6f31cb267b8bdc50043c2f210909c257
Hans-Christian Otto <c.otto@suora.com>
2023-06-27 09:59:45 +0200

    display value below form field

--- src/components/TimeEntryForm.tsx
+++ src/components/TimeEntryForm.tsx
@@ -1,13 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 const TimeEntryForm: React.FunctionComponent = () => {
+  const [inputValue, setInputValue] = useState("");
+
   const handleSubmit: React.FormEventHandler<HTMLFormElement> = (event) => {
     event.preventDefault();
     console.log("Hello World");
   };
 
+  const handleChange: React.ChangeEventHandler<HTMLInputElement> = (event) => {
+    setInputValue(event.target.value);
+  };
+
   return (
     <form onSubmit={handleSubmit}>
+      <input onChange={handleChange} />
+      {inputValue}
       <button type="submit">Absenden</button>
     </form>
   );
