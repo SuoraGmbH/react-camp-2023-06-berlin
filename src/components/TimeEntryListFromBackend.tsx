3dcfd66cbef4edce30e53c357cae9259236851ca
Hans-Christian Otto <c.otto@suora.com>
2023-06-28 13:31:04 +0200

    add zod

--- src/components/TimeEntryListFromBackend.tsx
+++ src/components/TimeEntryListFromBackend.tsx
@@ -1,6 +1,16 @@
 import React, { useEffect, useState } from "react";
 import TimeEntry from "../domain/TimeEntry";
 import TimeEntryList from "./TimeEntryList";
+import { z } from "zod";
+
+const TimeEntryBackendSchema = z.object({
+  id: z.string(),
+  comment: z.string(),
+  start: z.string(),
+  end: z.string().datetime(),
+});
+
+const TimeEntryResponseSchema = z.array(TimeEntryBackendSchema);
 
 const TimeEntryListFromBackend: React.FunctionComponent = () => {
   const [timeEntries, setTimeEntries] = useState<TimeEntry[]>();
@@ -8,15 +18,17 @@ const TimeEntryListFromBackend: React.FunctionComponent = () => {
   useEffect(() => {
     fetch("http://localhost:3001/timeEntries")
       .then((response) => response.json())
-      .then((timeEntries) =>
-        timeEntries.map((timeEntry: any) => {
-          return {
+      .then((timeEntries: unknown) => {
+        const parsedTimeEntries = TimeEntryResponseSchema.parse(timeEntries);
+
+        return parsedTimeEntries.map(
+          (timeEntry): TimeEntry => ({
             ...timeEntry,
             start: new Date(timeEntry.start),
             end: new Date(timeEntry.end),
-          };
-        })
-      )
+          })
+        );
+      })
       .then(setTimeEntries);
   }, []);
 
