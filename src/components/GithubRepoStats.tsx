05b171988d03eea3d8c3832c16c037ff661b00bf
Hans-Christian Otto <c.otto@suora.com>
2023-06-28 10:35:15 +0200

    Add error handling

--- src/components/GithubRepoStats.tsx
+++ src/components/GithubRepoStats.tsx
@@ -2,13 +2,29 @@ import React, { useEffect, useState } from "react";
 
 const GithubRepoStats: React.FunctionComponent = () => {
   const [stargazersCount, setStargazersCount] = useState<number>();
+  const [error, setError] = useState<Error>();
 
   useEffect(() => {
-    fetch("https://api.github.com/repos/facebook/react")
-      .then((response) => response.json())
+    fetch("https://api.github.com/repos/lacebook/react")
+      .then((response) => {
+        if (response.status !== 200) {
+          throw new Error("Received http status " + response.status);
+        }
+
+        return response.json();
+      })
       .then((body) => body.stargazers_count)
-      .then(setStargazersCount);
+      .then(setStargazersCount)
+      .catch(setError);
   }, []);
+
+  if (error !== undefined) {
+    return (
+      <pre>
+        <b>{error.name}</b>: {error.message}
+      </pre>
+    );
+  }
 
   if (stargazersCount === undefined) {
     return <div>Loading…</div>;
