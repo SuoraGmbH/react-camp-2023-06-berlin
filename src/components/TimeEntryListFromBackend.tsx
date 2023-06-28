5022ea631582a461946eecd50d8256a48e83f7a1
Hans-Christian Otto <c.otto@suora.com>
2023-06-28 15:25:43 +0200

    ooops

--- src/components/TimeEntryListFromBackend.tsx
+++ src/components/TimeEntryListFromBackend.tsx
@@ -3,16 +3,15 @@ import TimeEntryList from "./TimeEntryList";
 import fetchTimeEntries from "../api/fetchTimeEntries";
 import { useQuery } from "@tanstack/react-query";
 import { useGetTimeEntries } from "../api/time-entries";
-import { data } from "msw";
 
 const TimeEntryListFromBackend: React.FunctionComponent = () => {
   const { data: response } = useGetTimeEntries();
 
-  if (data === undefined) {
+  if (response === undefined) {
     return <div>Loading…</div>;
   }
 
-  return <TimeEntryList timeEntries={data.data} />;
+  return <TimeEntryList timeEntries={response.data} />;
 };
 
 export default TimeEntryListFromBackend;
