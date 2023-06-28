86f77b0471e228804efddcfe38d082de8e704664
Hans-Christian Otto <c.otto@suora.com>
2023-06-28 15:26:57 +0200

    Revert "ooops"
    This reverts commit 5022ea631582a461946eecd50d8256a48e83f7a1.

--- src/components/TimeEntryListFromBackend.tsx
+++ src/components/TimeEntryListFromBackend.tsx
@@ -3,15 +3,16 @@ import TimeEntryList from "./TimeEntryList";
 import fetchTimeEntries from "../api/fetchTimeEntries";
 import { useQuery } from "@tanstack/react-query";
 import { useGetTimeEntries } from "../api/time-entries";
+import { data } from "msw";
 
 const TimeEntryListFromBackend: React.FunctionComponent = () => {
   const { data: response } = useGetTimeEntries();
 
-  if (response === undefined) {
+  if (data === undefined) {
     return <div>Loading…</div>;
   }
 
-  return <TimeEntryList timeEntries={response.data} />;
+  return <TimeEntryList timeEntries={data.data} />;
 };
 
 export default TimeEntryListFromBackend;
